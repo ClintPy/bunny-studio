50e61fa0e00e7b87128675bc7b27ecfd2b2bb902
ClintPy <cmaruti93@gmail.com>
2019-12-11 17:00:12 +0300

    [Finishes #170235067] SetsUp Database

--- build/src/server.js
+++ build/src/server.js
@@ -8,14 +8,35 @@ var _express = require('express');
 
 var _express2 = _interopRequireDefault(_express);
 
+var _mongoose = require('mongoose');
+
+var _mongoose2 = _interopRequireDefault(_mongoose);
+
 var _bodyParser = require('body-parser');
 
 var _bodyParser2 = _interopRequireDefault(_bodyParser);
 
+var _dotenv = require('dotenv');
+
+var _dotenv2 = _interopRequireDefault(_dotenv);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 // server.js
+_dotenv2.default.config();
+
 var app = (0, _express2.default)();
+
+var DB_NAME = process.env.DB_NAME || 'root'; // In production I'll omit the hard-coded name
+var DB_PWD = process.env.DB_PWD || 'eastood';
+
+var DEV_CONNECTION_URI = 'mongodb+srv://' + DB_NAME + ':' + DB_PWD + '@cluster0-nsnkg.mongodb.net/dev?retryWrites=true&w=majority';
+
+// Connect to Mongodb
+_mongoose2.default.connect(DEV_CONNECTION_URI, { useUnifiedTopology: true, useNewUrlParser: true });
+var db = _mongoose2.default.connection;
+
+if (!db) console.log("Error Connecting to DB!");else console.log("DB Connected Successfully!");
 
 app.use(_bodyParser2.default.json());
 
--- src/server.js
+++ src/server.js
@@ -1,8 +1,26 @@
 // server.js
 import express from 'express';
+import mongoose from "mongoose";
 import bodyPaser from 'body-parser';
+import dotenv from 'dotenv';
+
+dotenv.config()
 
 const app = express()
+
+const DB_NAME = process.env.DB_NAME || 'root' // In production I'll omit the hard-coded name
+const DB_PWD = process.env.DB_PWD || 'eastood'
+
+const DEV_CONNECTION_URI = `mongodb+srv://${DB_NAME}:${DB_PWD}@cluster0-nsnkg.mongodb.net/dev?retryWrites=true&w=majority`
+
+// Connect to Mongodb
+mongoose.connect(DEV_CONNECTION_URI, { useUnifiedTopology: true,useNewUrlParser: true})
+const db = mongoose.connection
+
+if(!db)
+    console.log("Error Connecting to DB!")
+else
+    console.log("DB Connected Successfully!")
 
 app.use(bodyPaser.json())
 
