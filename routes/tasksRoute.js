4e0b6445fc680df23f253fe31adb7e53fd827a96
ClintPy <cmaruti93@gmail.com>
2019-12-11 19:13:38 +0300

    [Finishes

--- routes/tasksRoute.js
+++ routes/tasksRoute.js
@@ -2,3 +2,8 @@ import express from 'express';
 import tasksController from '../controllers/tasks';
 
 const router = new express.Router;
+
+router.get('/api/v1/tasks/:id', tasksController.userByTasks);
+router.post('/api/v1/tasks', tasksController.createTask);
+
+export default router
